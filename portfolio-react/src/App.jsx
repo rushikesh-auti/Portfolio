cbd326b05943c95d5e78d37e51248fa893747b09
Rushikesh Auti <autirushikesh758@gmail.com>
2026-08-12 12:42:02 +0530

    Create projects components

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Hero />
         <About />
         <Skills />
+        <Projects />
       </main>
     </div>
   );
